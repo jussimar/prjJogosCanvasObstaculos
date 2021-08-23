0116064ec0833abd19cf9db05a98a00320c265c0
jussimar <jussimar@jussimarleal.com.br>
2021-08-23 19:23:35 -0300

    criando figura do personagem

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ window.onload = function(){
 
 function iniciarJogo(){
     areaJogo.start();
+    personagem = personagem('#f00',10,120,60,60);
 }
 
 let areaJogo = {
@@ -14,4 +15,14 @@ let areaJogo = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
+}
+
+function personagem(cor, x, y, largura, altura, ){
+    this.altura = altura,
+    this.largura = largura,
+    this.x = x,
+    this.y = y,
+    contexto = areaJogo.context;
+    contexto.fillStyle = cor,
+    contexto.fillRect(this.x, this.y, this.altura, this.largura);
 }
